b014ec34c085bfeee4ddafdfa3702d8f9da06fd1
nagano <shionaga0214@gmail.com>
2024-11-14 16:01:09 +0900

    Add correction

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,10 @@
 #!/usr/bin/env ruby
 (1..20).each do |n|
-  case
-  when n % 3 == 0 && n % 5 == 0
+  if n % 3 == 0 && n % 5 == 0
     puts "FizzBuzz"
-  when n % 5 == 0
+  elsif n % 5 == 0
     puts "Buzz"
-  when n % 3 == 0
+  elsif n % 3 == 0
     puts "Fizz"
   else
     puts n

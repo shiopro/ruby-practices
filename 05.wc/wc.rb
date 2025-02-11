6887f5cce9a84f22c4135a30ac5849084addd7b0
nagano <shionaga0214@gmail.com>
2025-02-11 13:22:02 +0900

    コマンドオプションの設定

---
+++ 05.wc/wc.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+opt = OptionParser.new
+options = {}
+
+opt.on('-l', '--lines') do
+  options[:lines] = true
+end
+
+opt.on('-w', '--words') do
+  options[:words] = true
+end
+
+opt.on('-c', '--bytes') do
+  options[:bytes] = true
+end
+
+opt.parse!(ARGV)

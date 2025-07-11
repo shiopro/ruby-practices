eba000f7099a513a4024c5a19cf77428ba7b6f28
nagano <shionaga0214@gmail.com>
2025-07-11 17:16:19 +0900

    frameに対する処理のためFrameクラスに移動

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -22,4 +22,25 @@ class Frame
   def spare?
     [@first_shot, @second_shot].map(&:score).sum == 10 && !strike?
   end
+
+  def bonus_score_except_last_frame(frame, next_frame, next_next_frame)
+    return 0 unless next_frame
+    return spare_bonus(next_frame) if frame.spare?
+    return strike_bonus(next_frame, next_next_frame) if frame.strike?
+
+    0
+  end
+
+  def spare_bonus(next_frame)
+    next_frame.first_shot.score
+  end
+
+  def strike_bonus(next_frame, next_next_frame)
+    if next_frame.strike?
+      next_shot = next_next_frame&.first_shot&.score || next_frame.second_shot.score || 0
+      10 + next_shot
+    else
+      next_frame.first_shot.score + next_frame.second_shot.score
+    end
+  end
 end
--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -7,27 +7,6 @@ class Game
     @frames = frames
   end
 
-  def bonus_score_except_last_frame(frame, next_frame, next_next_frame)
-    return 0 unless next_frame
-    return spare_bonus(next_frame) if frame.spare?
-    return strike_bonus(next_frame, next_next_frame) if frame.strike?
-
-    0
-  end
-
-  def spare_bonus(next_frame)
-    next_frame.first_shot.score
-  end
-
-  def strike_bonus(next_frame, next_next_frame)
-    if next_frame.strike?
-      next_shot = next_next_frame&.first_shot&.score || next_frame.second_shot.score || 0
-      10 + next_shot
-    else
-      next_frame.first_shot.score + next_frame.second_shot.score
-    end
-  end
-
   def total_score
     point = 0
     @frames.each_with_index do |frame, index|
@@ -38,7 +17,7 @@ class Game
       next_next_frame = @frames[index + 2]
 
       if next_frame && (frame.strike? || frame.spare?)
-        bonus = bonus_score_except_last_frame(frame, next_frame, next_next_frame)
+        bonus = frame.bonus_score_except_last_frame(frame, next_frame, next_next_frame)
         point += bonus
       end
     end

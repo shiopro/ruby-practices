a2fed6b6886426a4596f5d7fa1c843106d814d4c
nagano <shionaga0214@gmail.com>
2025-07-13 11:11:39 +0900

    index チェックで十分なため next_frame の条件を削除

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -11,14 +11,11 @@ class Game
     @frames.each_with_index.sum do |frame, index|
       point = frame.score
 
-      if index < 9
+      if index < 9 && (frame.strike? || frame.spare?)
         next_frame = @frames[index + 1]
         next_next_frame = @frames[index + 2]
-
-        if next_frame && (frame.strike? || frame.spare?)
-          bonus = frame.bonus_score_except_last_frame(frame, next_frame, next_next_frame)
-          point += bonus
-        end
+        bonus = frame.bonus_score_except_last_frame(frame, next_frame, next_next_frame)
+        point += bonus
       end
       point
     end
